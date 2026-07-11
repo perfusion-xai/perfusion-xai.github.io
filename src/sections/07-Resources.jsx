5ba5f7f8712a1ebd01d5950d4533b0f1d25bfa39
perfusion-xai <reachninadaithal+cbf@gmail.com>
2026-07-12 00:01:18 +0530

    Update 07-Resources.jsx
    Added biorxiv link

--- src/sections/07-Resources.jsx
+++ src/sections/07-Resources.jsx
@@ -2,10 +2,10 @@ import Section from "../components/Section.jsx";
 
 const LINKS = [
   {
-    title: "Preprint",
-    sub: "arXiv (eess.IV)",
-    href: "#",
-    note: "Coming soon",
+    title: "DATA-DRIVEN IDENTIFICATION OF SEX DIFFERENCES IN CEREBRAL BLOOD FLOW USING ARTERIAL SPIN LABELLING AND EXPLAINABLE ARTIFICIAL INTELLIGENCE",
+    sub: "biorxiv",
+    href: "https://doi.org/10.64898/2026.07.05.736642",
+    note: "DOI: https://doi.org/10.64898/2026.07.05.736642",
   },
   {
     title: "Code",
